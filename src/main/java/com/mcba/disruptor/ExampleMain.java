22122809acefae0c5d67af3570aa6b2729de9394
lanyangyang <yufeng0528@126.com>
2018-03-15 09:47:22 +0800

    ExampleMain 修改

--- src/main/java/com/mcba/disruptor/ExampleMain.java
+++ src/main/java/com/mcba/disruptor/ExampleMain.java
@@ -1,41 +1,53 @@
 package com.mcba.disruptor;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import com.lmax.disruptor.EventHandler;
 import com.lmax.disruptor.RingBuffer;
+import com.lmax.disruptor.YieldingWaitStrategy;
 import com.lmax.disruptor.dsl.Disruptor;
+import com.lmax.disruptor.dsl.ProducerType;
+import com.mcba.disruptor.eventhandler.PrintCarEventHandler;
 import com.mcba.disruptor.eventhandler.SetColorCarEventHandler;
 import com.mcba.disruptor.eventhandler.SetPowerCarEventHandler;
 import com.mcba.disruptor.eventhandler.SetWheelCarEventHandler;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 /**
  * Created by Hichame EL KHALFI on 09/06/2015.
  */
 public class ExampleMain {
 
-    public static void main(String[] args) {
+    @SuppressWarnings("unchecked")
+	public static void main(String[] args) {
         // 1 - Executor that will be used to construct new threads for consumers
-        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() / 2);
+    	// https://stackoverflow.com/questions/8309735/disruptor-eventhandlers-not-invoked
+        ExecutorService executorService = Executors.newCachedThreadPool();
 
         // 2 - Specify the size of the ring buffer, must be power of 2.
-        int bufferSize = 2048;
+        int bufferSize = 64;
 
         // 3 - initialize the Disruptor object
-        Disruptor<CarEvent> disruptor = new Disruptor(CarEvent::new, bufferSize, executorService);//java 8 flavor
+        Disruptor<CarEvent> disruptor = new Disruptor<CarEvent>(new CarEventFactory(), bufferSize, executorService,
+        		ProducerType.SINGLE, new YieldingWaitStrategy());//java 8 flavor
 
-        disruptor.handleEventsWith(new SetColorCarEventHandler())
-                .then(new SetPowerCarEventHandler())
-                .then(new SetWheelCarEventHandler())
-                .then((CarEvent event, long sequence, boolean endOfBatch) ->
-                        System.out.println(String.format("id [%d] with %s", sequence, event.get())));
+        //http://blog.csdn.net/u011499747/article/details/78384292
+        EventHandler<CarEvent> setColor = new SetColorCarEventHandler();
+        EventHandler<CarEvent> setPower = new SetPowerCarEventHandler();
+        EventHandler<CarEvent> setWheel = new SetWheelCarEventHandler();
+        EventHandler<CarEvent> print = new PrintCarEventHandler();
+        
+        disruptor.handleEventsWith(setColor, setWheel);
+        disruptor.after(setColor, setWheel).then(setPower).then(print);
+//        disruptor.after(setColor).then(print);
+//        disruptor.after(setWheel, setPower).then(print);
 
         disruptor.start();
 
         // Get the ring buffer from the Disruptor to be used for publishing.
         final RingBuffer<CarEvent> ringBuffer = disruptor.getRingBuffer();
 
-        for (long l = 0; l < 1000; l++) {
+        for (long l = 0; l < 1; l++) {
             // 1 - get next car sequence (sequence is internal ringbuffer counter)
             long seq = ringBuffer.next();
 
@@ -48,5 +60,6 @@ public class ExampleMain {
             //4 - publish the event using it sequence.
             ringBuffer.publish(seq);
         }
+        
     }
 }
